cbfae0b9d066918923149361a0ae5483c71ac004
Omaralsaabi <prof.omaralsaabi@gmail.com>
2023-05-09 12:13:11 +0300

    add html

--- de/views.py
+++ de/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+def index(request):
+    html = "<html><body><h1>السلام عليكم</h1></body></html>"
+    return HttpResponse(html)
